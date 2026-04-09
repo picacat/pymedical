237537cfeeebab163450e666d5fcfb5dd5242544
John Huang <pica.cat@gmail.com>
2026-04-09 12:44:19 +0800

    2026-04-09 update(37)

--- pymedical.win32.bat
+++ pymedical.win32.bat
@@ -3,13 +3,9 @@
 cd /d "%~dp0"
 
 :: 檢查 venv 是否存在
-if not exist ".\venv\Scripts\activate.bat" (
-    echo [錯誤] 找不到虛擬環境，請先執行安裝腳本。
-    pause
-    exit
+if exist "\pymedical\venv\Scripts\activate.bat" (
+    call "\pymedical\venv\Scripts\activate.bat"
 )
 
-:: 啟用環境並執行
-call ".\venv\Scripts\activate.bat"
 start /min py -3-32 pymedical.py
 
