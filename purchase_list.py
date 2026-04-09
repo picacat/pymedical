3847f296b45c7adebff093bb65a7ef20b6ed70a6
John Huang <pica.cat@gmail.com>
2026-04-09 09:59:24 +0800

    2026-04-09 update(6)

--- purchase_list.py
+++ purchase_list.py
@@ -1,25 +1,28 @@
 # -*- coding: UTF-8 -*-
-# 櫃台購藥 2014.09.22
+# 櫃台購藥 2024.09.22
 
-from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QMessageBox, QPushButton, QFileDialog
 import datetime
 
-from libs import class_utils
-from libs import ui_utils
-from libs import system_utils
-from libs import string_utils
-from libs import number_utils
-from libs import personnel_utils
-from libs import export_utils
-from libs import printer_utils
-from libs import dialog_utils
-from libs import stock_utils
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QPushButton
+
+from libs import (
+    class_utils,
+    dialog_utils,
+    export_utils,
+    number_utils,
+    personnel_utils,
+    printer_utils,
+    stock_utils,
+    string_utils,
+    system_utils,
+    ui_utils,
+)
 
 
 # 櫃台購藥
 class PurchaseList(QtWidgets.QMainWindow):
-    program_name = '櫃台購藥'
+    program_name = "櫃台購藥"
 
     # 初始化
     def __init__(self, parent=None, *args):
@@ -77,19 +80,41 @@ class PurchaseList(QtWidgets.QMainWindow):
         self.ui.action_close.triggered.connect(self.close_purchase_list)
         self.ui.action_print_receipt.triggered.connect(self._print_receipt)
         self.ui.action_export_excel.triggered.connect(self._export_to_excel)
-        self.ui.tableWidget_purchase_list.doubleClicked.connect(self.open_medical_record)
+        self.ui.tableWidget_purchase_list.doubleClicked.connect(
+            self.open_medical_record
+        )
 
     def _set_permission(self):
-        if self.user_name == '超級使用者':
+        if self.user_name == "超級使用者":
             return
 
-        if personnel_utils.get_permission(self.database, self.program_name, '購買商品', self.user_name) != 'Y':
+        if (
+            personnel_utils.get_permission(
+                self.database, self.program_name, "購買商品", self.user_name
+            )
+            != "Y"
+        ):
             self.ui.action_purchase.setEnabled(False)
-        if personnel_utils.get_permission(self.database, self.program_name, '購藥明細', self.user_name) != 'Y':
+        if (
+            personnel_utils.get_permission(
+                self.database, self.program_name, "購藥明細", self.user_name
+            )
+            != "Y"
+        ):
             self.ui.action_open_record.setEnabled(False)
-        if personnel_utils.get_permission(self.database, self.program_name, '資料刪除', self.user_name) != 'Y':
+        if (
+            personnel_utils.get_permission(
+                self.database, self.program_name, "資料刪除", self.user_name
+            )
+            != "Y"
+        ):
             self.ui.action_delete_record.setEnabled(False)
-        if personnel_utils.get_permission(self.database, '系統作業', '關閉匯出功能', self.user_name) == 'Y':
+        if (
+            personnel_utils.get_permission(
+                self.database, "系統作業", "關閉匯出功能", self.user_name
+            )
+            == "Y"
+        ):
             self.ui.action_export_excel.setEnabled(False)
 
     # 設定欄位寬度
@@ -100,27 +125,29 @@ class PurchaseList(QtWidgets.QMainWindow):
 
     # 讀取病歷
     def _get_sql(self):
-        dialog = dialog_utils.get_dialog_purchase_list(self.ui, self.database, self.system_settings)
-        if self.dialog_setting['dialog_executed']:
-            dialog.ui.dateEdit_start_date.setDate(self.dialog_setting['start_date'])
-            dialog.ui.dateEdit_end_date.setDate(self.dialog_setting['end_date'])
-            dialog.ui.comboBox_period.setCurrentText(self.dialog_setting['period'])
-            dialog.ui.comboBox_cashier.setCurrentText(self.dialog_setting['cashier'])
-            dialog.ui.comboBox_doctor.setCurrentText(self.dialog_setting['therapist'])
-            dialog.ui.comboBox_massager.setCurrentText(self.dialog_setting['massager'])
+        dialog = dialog_utils.get_dialog_purchase_list(
+            self.ui, self.database, self.system_settings
+        )
+        if self.dialog_setting["dialog_executed"]:
+            dialog.ui.dateEdit_start_date.setDate(self.dialog_setting["start_date"])
+            dialog.ui.dateEdit_end_date.setDate(self.dialog_setting["end_date"])
+            dialog.ui.comboBox_period.setCurrentText(self.dialog_setting["period"])
+            dialog.ui.comboBox_cashier.setCurrentText(self.dialog_setting["cashier"])
+            dialog.ui.comboBox_doctor.setCurrentText(self.dialog_setting["therapist"])
+            dialog.ui.comboBox_massager.setCurrentText(self.dialog_setting["massager"])
 
         result = dialog.exec_()
-        self.dialog_setting['dialog_executed'] = True
-        self.dialog_setting['start_date'] = dialog.ui.dateEdit_start_date.date()
-        self.dialog_setting['end_date'] = dialog.ui.dateEdit_end_date.date()
-        self.dialog_setting['period'] = dialog.comboBox_period.currentText()
-        self.dialog_setting['cashier'] = dialog.comboBox_cashier.currentText()
-        self.dialog_setting['therapist'] = dialog.comboBox_doctor.currentText()
-        self.dialog_setting['massager'] = dialog.comboBox_massager.currentText()
+        self.dialog_setting["dialog_executed"] = True
+        self.dialog_setting["start_date"] = dialog.ui.dateEdit_start_date.date()
+        self.dialog_setting["end_date"] = dialog.ui.dateEdit_end_date.date()
+        self.dialog_setting["period"] = dialog.comboBox_period.currentText()
+        self.dialog_setting["cashier"] = dialog.comboBox_cashier.currentText()
+        self.dialog_setting["therapist"] = dialog.comboBox_doctor.currentText()
+        self.dialog_setting["massager"] = dialog.comboBox_massager.currentText()
 
         sql = dialog.get_sql()
-        start_date = dialog.ui.dateEdit_start_date.date().toString('yyyy-MM-dd')
-        end_date = dialog.ui.dateEdit_end_date.date().toString('yyyy-MM-dd')
+        start_date = dialog.ui.dateEdit_start_date.date().toString("yyyy-MM-dd")
+        end_date = dialog.ui.dateEdit_end_date.date().toString("yyyy-MM-dd")
 
         dialog.close_all()
         dialog.deleteLater()
@@ -135,7 +162,7 @@ class PurchaseList(QtWidgets.QMainWindow):
         if self.sql is None:
             return
 
-        self.ui.label_data_period.setText(f'資料期間: {start_date} 至 {end_date}')
+        self.ui.label_data_period.setText(f"資料期間: {start_date} 至 {end_date}")
         self._read_purchase_list()
 
     def _read_purchase_list(self):
@@ -147,12 +174,12 @@ class PurchaseList(QtWidgets.QMainWindow):
         self._set_tool_button()
 
     def read_purchase_today(self):
-        start_date = datetime.datetime.now().strftime('%Y-%m-%d')
-        end_date = datetime.datetime.now().strftime('%Y-%m-%d')
-        self.ui.label_data_period.setText(f'資料期間: {start_date} 至 {end_date}')
+        start_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        end_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        self.ui.label_data_period.setText(f"資料期間: {start_date} 至 {end_date}")
 
-        start_date += ' 00:00:00'
-        end_date += ' 23:59:59'
+        start_date += " 00:00:00"
+        end_date += " 23:59:59"
 
         self.sql = f'''
             SELECT * FROM cases
@@ -177,69 +204,65 @@ class PurchaseList(QtWidgets.QMainWindow):
         self._set_permission()
 
     def _set_table_data(self, row_no, row):
-        content = self._get_purchase_content(row['CaseKey'])
-        discount_fee = number_utils.get_integer(row['DiscountFee'])
+        content = self._get_purchase_content(row["CaseKey"])
+        discount_fee = number_utils.get_integer(row["DiscountFee"])
 
         purchase_row = [
-            string_utils.xstr(row['CaseKey']),
-            string_utils.xstr(row['CaseDate']),
-            string_utils.xstr(row['Period']),
-            string_utils.xstr(row['PatientKey']),
-            string_utils.xstr(row['Name']),
+            string_utils.xstr(row["CaseKey"]),
+            string_utils.xstr(row["CaseDate"]),
+            string_utils.xstr(row["Period"]),
+            string_utils.xstr(row["PatientKey"]),
+            string_utils.xstr(row["Name"]),
             content,
-            number_utils.get_integer(row['SelfTotalFee']),
+            number_utils.get_integer(row["SelfTotalFee"]),
             discount_fee,
-            number_utils.get_integer(row['TotalFee']),
-            string_utils.xstr(row['Cashier']),
-            string_utils.xstr(row['Doctor']),
-            string_utils.xstr(row['Massager']),
+            number_utils.get_integer(row["TotalFee"]),
+            string_utils.xstr(row["Cashier"]),
+            string_utils.xstr(row["Doctor"]),
+            string_utils.xstr(row["Massager"]),
         ]
 
         for col_no in range(len(purchase_row)):
             item = QtWidgets.QTableWidgetItem()
             item.setData(QtCore.Qt.EditRole, purchase_row[col_no])
-            self.ui.tableWidget_purchase_list.setItem(
-                row_no, col_no, item
-            )
+            self.ui.tableWidget_purchase_list.setItem(row_no, col_no, item)
             if col_no in [3, 6, 7, 8]:
-                self.ui.tableWidget_purchase_list.item(
-                    row_no, col_no).setTextAlignment(
+                self.ui.tableWidget_purchase_list.item(row_no, col_no).setTextAlignment(
                     QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
                 )
             elif col_no in [2]:
-                self.ui.tableWidget_purchase_list.item(
-                    row_no, col_no).setTextAlignment(
+                self.ui.tableWidget_purchase_list.item(row_no, col_no).setTextAlignment(
                     QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter
                 )
 
             if discount_fee > 0:
                 self.ui.tableWidget_purchase_list.item(row_no, col_no).setForeground(
-                    QtGui.QColor('red')
+                    QtGui.QColor("red")
                 )
 
     def _get_purchase_content(self, case_key):
-        sql = f'''
+        sql = f"""
             SELECT * FROM prescript
             WHERE
                 CaseKey = {case_key}
             ORDER BY PrescriptKey
-        '''
+        """
         rows = self.database.select_record(sql)
         content = []
         for row in rows:
-            medicine_name = string_utils.xstr(row['MedicineName'])
-            quantity = string_utils.xstr(row['Dosage'])
-            unit = string_utils.xstr(row['Unit'])
-            content.append(f'{medicine_name} ({quantity}{unit})')
+            medicine_name = string_utils.xstr(row["MedicineName"])
+            quantity = string_utils.xstr(row["Dosage"])
+            unit = string_utils.xstr(row["Unit"])
+            content.append(f"{medicine_name} ({quantity}{unit})")
 
-        return ', '.join(content)
+        return ", ".join(content)
 
     def _calculate_total(self):
         row_count = self.ui.tableWidget_purchase_list.rowCount()
         self_total_fee, discount_fee, total_fee = 0, 0, 0
 
         for row_no in range(row_count):
-            if self.ui.tableWidget_purchase_list.item(row_no, 4).text() == '合計':
+            if self.ui.tableWidget_purchase_list.item(row_no, 4).text() == "合計":
                 continue
 
             self_total_fee += number_utils.get_integer(
@@ -253,8 +276,11 @@ class PurchaseList(QtWidgets.QMainWindow):
             )
 
         total_record = [
-            None, None, None, None,
-            '合計',
+            None,
+            None,
+            None,
+            None,
+            "合計",
             None,
             string_utils.xstr(self_total_fee),
             string_utils.xstr(discount_fee),
@@ -263,7 +289,7 @@ class PurchaseList(QtWidgets.QMainWindow):
 
         row_no = self._get_total_row_no()
         if row_no is None:
-            self.ui.tableWidget_purchase_list.setRowCount(row_count+1)
+            self.ui.tableWidget_purchase_list.setRowCount(row_count + 1)
         else:
             row_count = row_no
 
@@ -271,14 +297,12 @@ class PurchaseList(QtWidgets.QMainWindow):
         font.setBold(True)
         for col_no in range(len(total_record)):
             self.ui.tableWidget_purchase_list.setItem(
-                row_count, col_no,
-                QtWidgets.QTableWidgetItem(total_record[col_no])
+                row_count, col_no, QtWidgets.QTableWidgetItem(total_record[col_no])
             )
             if col_no in [6, 7, 8]:
                 self.ui.tableWidget_purchase_list.item(
-                    row_count, col_no).setTextAlignment(
-                    QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
-                )
+                    row_count, col_no
+                ).setTextAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
             self.ui.tableWidget_purchase_list.item(row_count, col_no).setFont(font)
 
     def _get_total_row_no(self):
@@ -286,7 +310,7 @@ class PurchaseList(QtWidgets.QMainWindow):
 
         row_count = self.ui.tableWidget_purchase_list.rowCount()
         for i in range(row_count):
-            if self.ui.tableWidget_purchase_list.item(i, 4).text() == '合計':
+            if self.ui.tableWidget_purchase_list.item(i, 4).text() == "合計":
                 row_no = i
                 break
 
@@ -294,13 +318,13 @@ class PurchaseList(QtWidgets.QMainWindow):
 
     def delete_purchase(self):
         case_key = self.table_widget_purchase_list.field_value(0)
-        if case_key in [None, '']:
+        if case_key in [None, ""]:
             return
 
         name = self.table_widget_purchase_list.field_value(4)
         msg_box = QMessageBox()
         msg_box.setIcon(QMessageBox.Warning)
-        msg_box.setWindowTitle('刪除購藥資料')
+        msg_box.setWindowTitle("刪除購藥資料")
         msg_box.setText(f"""
             <font size='4' color='red'>
                 <b>確定刪除<font color='blue'> {name} </font>的購藥資料?</b>
@@ -317,27 +341,27 @@ class PurchaseList(QtWidgets.QMainWindow):
         if case_key is None:
             return
 
-        if self.system_settings.field('調整庫存量') == '即時調整':
+        if self.system_settings.field("調整庫存量") == "即時調整":
             stock_utils.restore_prescript_quantity(self.database, case_key)
 
-        self.database.delete_record('prescript', 'CaseKey', case_key)
-        self.database.delete_record('cases', 'CaseKey', case_key)
-        self.database.delete_record('wait', 'CaseKey', case_key)
+        self.database.delete_record("prescript", "CaseKey", case_key)
+        self.database.delete_record("cases", "CaseKey", case_key)
+        self.database.delete_record("wait", "CaseKey", case_key)
         current_row = self.ui.tableWidget_purchase_list.currentRow()
         self.ui.tableWidget_purchase_list.removeRow(current_row)
         self._calculate_total()
 
     def open_medical_record(self):
-        self.purchase_mode = '購藥明細'
+        self.purchase_mode = "購藥明細"
 
         case_key = self.table_widget_purchase_list.field_value(0)
-        if case_key in [None, '']:
+        if case_key in [None, ""]:
             return
 
-        self.parent.open_medical_record(case_key, '櫃台購藥')
+        self.parent.open_medical_record(case_key, "櫃台購藥")
 
     def get_case_key(self):
-        if self.purchase_mode == '購買商品':
+        if self.purchase_mode == "購買商品":
             case_key = None
         else:
             case_key = self.table_widget_purchase_list.field_value(0)
@@ -347,7 +371,7 @@ class PurchaseList(QtWidgets.QMainWindow):
     # 重新顯示資料 call from pymedical (call from here is not working)
     def refresh_purchase(self, in_case_key):
         self._read_purchase_list()
-        if in_case_key in [None, '']:
+        if in_case_key in [None, ""]:
             return
 
         for row_no in range(self.ui.tableWidget_purchase_list.rowCount()):
@@ -361,7 +385,7 @@ class PurchaseList(QtWidgets.QMainWindow):
 
     # 輸入購物資料
     def _purchase(self):
-        self.purchase_mode = '購買商品'
+        self.purchase_mode = "購買商品"
 
         self.parent.open_purchase_tab()
 
@@ -375,39 +399,40 @@ class PurchaseList(QtWidgets.QMainWindow):
 
     def _print_receipt(self):
         case_key = self.table_widget_purchase_list.field_value(0)
-        if case_key in [None, '']:
+        if case_key in [None, ""]:
             return
 
         printer_utils.print_receipt_form(
-            self, self.database, self.system_settings, case_key, '選擇列印')
+            self, self.database, self.system_settings, case_key, "選擇列印"
+        )
 
     # 匯出自購藥 2019.07.01
     def _export_to_excel(self):
-        if self.dialog_setting['start_date'] is None:
+        if self.dialog_setting["start_date"] is None:
             return
 
         options = QFileDialog.Options()
-        start_date = self.dialog_setting['start_date'].toString('yyyy-MM-dd')
-        end_date = self.dialog_setting['end_date'].toString('yyyy-MM-dd')
-        period = self.dialog_setting['period']
-        title = f'{start_date}至{end_date}{period}自購藥報表'
+        start_date = self.dialog_setting["start_date"].toString("yyyy-MM-dd")
+        end_date = self.dialog_setting["end_date"].toString("yyyy-MM-dd")
+        period = self.dialog_setting["period"]
+        title = f"{start_date}至{end_date}{period}自購藥報表"
         excel_file_name, _ = QFileDialog.getSaveFileName(
             self.parent,
             "匯出自購藥",
-            f'{title}.xlsx',
-            "excel檔案 (*.xlsx);;Text Files (*.txt)", options=options
+            f"{title}.xlsx",
+            "excel檔案 (*.xlsx);;Text Files (*.txt)",
+            options=options,
         )
         if not excel_file_name:
             return
 
         export_utils.export_table_widget_to_excel(
-            excel_file_name, self.ui.tableWidget_purchase_list,
-            [0], [3, 6, 7, 8], title
+            excel_file_name, self.ui.tableWidget_purchase_list, [0], [3, 6, 7, 8], title
         )
 
         system_utils.show_message_box(
             QMessageBox.Information,
-            '資料匯出完成',
-            f'<h3>自購藥報表{excel_file_name}匯出完成.</h3>',
-            'Microsoft Excel 格式.'
+            "資料匯出完成",
+            f"<h3>自購藥報表{excel_file_name}匯出完成.</h3>",
+            "Microsoft Excel 格式.",
         )
