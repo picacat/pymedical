b9badc53e85bd845029d52dd5ea01796bfb204e7
John Huang <pica.cat@gmail.com>
2026-04-09 11:14:26 +0800

    2026-04-09 update(9)

--- system_update.py
+++ system_update.py
@@ -20,7 +20,7 @@ from PyQt5.QtWidgets import QFileDialog, QMessageBox, QPushButton
 from libs import class_utils, string_utils, system_utils, ui_utils, update_utils
 
 
-# 醫療系統更新
+# 醫療系統更新 2026-04-09 sync with github
 class SystemUpdate(QtWidgets.QDialog):
     # 初始化
     def __init__(self, parent=None, *args):
