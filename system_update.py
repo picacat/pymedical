79caf101416f90bf0e481703d65cdbe4ef992eee
John Huang <pica.cat@gmail.com>
2026-04-09 12:28:29 +0800

    2026-04-09 update(32)

--- system_update.py
+++ system_update.py
@@ -470,6 +470,7 @@ class SystemUpdate(QtWidgets.QDialog):
             self._run_git(["config", "user.email", "clinic@update.local"])
             self._run_git(["config", "user.name", "ClinicUser"])
             self._run_git(["config", "core.autocrlf", "false"])
+            self._run_git(["update-index", "--skip-worktree", "pymedical.win32.bat"])
 
         # 這裡只做 fetch，把雲端資訊抓回來儲存在 FETCH_HEAD
         # 絕對不要在這裡跑 update-ref 或 reset --hard
