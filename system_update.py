4eaceeea910d32066fa61616d6969c09931cba92
John Huang <pica.cat@gmail.com>
2026-04-12 07:02:04 +0800

    2026-04-12 update (2)

--- system_update.py
+++ system_update.py
@@ -605,6 +605,7 @@ class SystemUpdate(QtWidgets.QDialog):
 
         self._check_files(dropbox_file)
 
+    # 回傳更新記錄
     def _report_to_zoho_server(self):
         """將更新結果回報至 www.zoho.net.tw 的 MariaDB"""
         conn = None
