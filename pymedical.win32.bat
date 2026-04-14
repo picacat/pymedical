f1e2902510329b5be3d2f91fe7267463ed26187b
John Huang <pica.cat@gmail.com>
2026-04-15 07:51:13 +0800

    2026-04-15 v4

--- pymedical.win32.bat
+++ pymedical.win32.bat
@@ -1,8 +1,8 @@
 @echo off
-:: 切換到腳本所在的目錄，確保相對路徑正確.
+:: change current dir
 cd /d "%~dp0"
 
-:: 檢查 venv 是否存在
+:: verify venv exists
 if exist "\pymedical\venv\Scripts\activate.bat" (
     call "\pymedical\venv\Scripts\activate.bat"
 )
