f5dbbbb776eaa4ebb5a5a3e1ef1e08523fd22b83
John Huang <pica.cat@gmail.com>
2026-05-21 15:57:44 +0800

    2026-05-21 v2

--- statistics_ins_apply_year.py
+++ statistics_ins_apply_year.py
@@ -1,18 +1,28 @@
 # -*- coding: UTF-8 -*-
 
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QFileDialog, QMessageBox
-from PyQt5.QtGui import QPainter
-from PyQt5.QtChart import QChart, QChartView, QBarSet, QBarSeries, QBarCategoryAxis, QValueAxis
 import os
 
-from libs import class_utils
-from libs import ui_utils
-from libs import string_utils
-from libs import system_utils
-from libs import dialog_utils
-from libs import number_utils
-from libs import export_utils
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtChart import (
+    QBarCategoryAxis,
+    QBarSeries,
+    QBarSet,
+    QChart,
+    QChartView,
+    QValueAxis,
+)
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+
+from libs import (
+    class_utils,
+    dialog_utils,
+    export_utils,
+    number_utils,
+    string_utils,
+    system_utils,
+    ui_utils,
+)
 
 
 # 健保申報分列項目表 2025-05-14
@@ -48,7 +58,8 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
         system_utils.set_css(self, self.system_settings)
         system_utils.center_window(self)
         self.table_widget_ins_apply = class_utils.get_table_widget(
-            self.ui.tableWidget_ins_apply, self.database)
+            self.ui.tableWidget_ins_apply, self.database
+        )
         self._set_table_width()
 
     # 設定信號
@@ -72,10 +83,12 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
 
     # 讀取病歷
     def open_dialog(self):
-        dialog = dialog_utils.get_dialog_date_picker(self, self.database, self.system_settings, 'by_year')
+        dialog = dialog_utils.get_dialog_date_picker(
+            self, self.database, self.system_settings, "by_year"
+        )
 
-        if self.dialog_setting['dialog_executed']:
-            dialog.ui.comboBox_year.setCurrentText(self.dialog_setting['year'])
+        if self.dialog_setting["dialog_executed"]:
+            dialog.ui.comboBox_year.setCurrentText(self.dialog_setting["year"])
 
         if not dialog.exec_():
             dialog.deleteLater()
@@ -83,8 +96,8 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
 
         year = dialog.ui.comboBox_year.currentText()
 
-        self.dialog_setting['dialog_executed'] = True
-        self.dialog_setting['year'] = year
+        self.dialog_setting["dialog_executed"] = True
+        self.dialog_setting["year"] = year
 
         dialog.deleteLater()
 
@@ -92,11 +105,17 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
         self._create_bar_chart()
 
     def _start_calculate(self):
+        # self.ui.tableWidget_ins_apply.setRowCount(0)
+        # year = int(self.dialog_setting['year']) - 1911
+        # apply_date_list = [f'{year-1}12']
+        # for i in range(1, 12):
+        #     apply_date_list.append(f'{year}{i:02}')
+
         self.ui.tableWidget_ins_apply.setRowCount(0)
-        year = int(self.dialog_setting['year']) - 1911
-        apply_date_list = [f'{year-1}12']
-        for i in range(1, 12):
-            apply_date_list.append(f'{year}{i:02}')
+        year = int(self.dialog_setting["year"]) - 1911
+
+        # 直接產生今年 01 到 12 月的字串列表
+        apply_date_list = [f"{year}{i:02}" for i in range(1, 13)]
 
         for apply_date in apply_date_list:
             self._set_ins_apply_data(apply_date)
@@ -121,19 +140,19 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
                 ApplyDate = "{apply_date}"
         '''
         row = self.database.select_record(sql)[0]
-        if row['record_count'] == 0:
+        if row["record_count"] == 0:
             return
 
         ins_apply_data = [
             apply_date,
-            row['total_diag_count'],
-            row['total_diag_fee'],
-            row['total_drug_fee'],
-            row['total_pharmacy_fee'],
-            row['total_treat_fee'],
-            row['total_ins_total_fee'],
-            row['total_share_fee'],
-            row['total_ins_apply_fee'],
+            row["total_diag_count"],
+            row["total_diag_fee"],
+            row["total_drug_fee"],
+            row["total_pharmacy_fee"],
+            row["total_treat_fee"],
+            row["total_ins_total_fee"],
+            row["total_share_fee"],
+            row["total_ins_apply_fee"],
         ]
         self._set_table_data(ins_apply_data)
 
@@ -147,30 +166,54 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
             item.setData(QtCore.Qt.EditRole, string_utils.xstr(ins_apply_data[col_no]))
             self.ui.tableWidget_ins_apply.setItem(row_no, col_no, item)
             if col_no in [0]:
-                self.ui.tableWidget_ins_apply.item(
-                    row_no, col_no).setTextAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
+                self.ui.tableWidget_ins_apply.item(row_no, col_no).setTextAlignment(
+                    QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter
+                )
             else:
-                self.ui.tableWidget_ins_apply.item(
-                    row_no, col_no).setTextAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+                self.ui.tableWidget_ins_apply.item(row_no, col_no).setTextAlignment(
+                    QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
+                )
 
     def _calculate_total(self):
         total_diag_count, diag_fee, drug_fee, pharmacy_fee, treat_fee = 0, 0, 0, 0, 0
-        ins_apply_total, share_fee, ins_apply_fee  = 0, 0, 0
+        ins_apply_total, share_fee, ins_apply_fee = 0, 0, 0
 
         for row_no in range(self.ui.tableWidget_ins_apply.rowCount()):
-            total_diag_count += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 1).text())
-            diag_fee += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 2).text())
-            drug_fee += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 3).text())
-            pharmacy_fee += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 4).text())
-            treat_fee += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 5).text())
-            ins_apply_total += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 6).text())
-            share_fee += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 7).text())
-            ins_apply_fee += number_utils.get_integer(self.ui.tableWidget_ins_apply.item(row_no, 8).text())
+            total_diag_count += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 1).text()
+            )
+            diag_fee += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 2).text()
+            )
+            drug_fee += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 3).text()
+            )
+            pharmacy_fee += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 4).text()
+            )
+            treat_fee += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 5).text()
+            )
+            ins_apply_total += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 6).text()
+            )
+            share_fee += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 7).text()
+            )
+            ins_apply_fee += number_utils.get_integer(
+                self.ui.tableWidget_ins_apply.item(row_no, 8).text()
+            )
 
         total_row = [
-            '合計',
-            total_diag_count, diag_fee, drug_fee, pharmacy_fee, treat_fee,
-            ins_apply_total, share_fee, ins_apply_fee,
+            "合計",
+            total_diag_count,
+            diag_fee,
+            drug_fee,
+            pharmacy_fee,
+            treat_fee,
+            ins_apply_total,
+            share_fee,
+            ins_apply_fee,
         ]
         self._set_table_data(total_row)
 
@@ -190,19 +233,21 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
         row_count = self.ui.tableWidget_ins_apply.rowCount()
         for row in range(row_count - 1):  # 最後一列是「合計」，不統計
             month = self.ui.tableWidget_ins_apply.item(row, 0).text()  # 申報年月
-            item_text = self.ui.tableWidget_ins_apply.item(row, 8).text().replace(",", "").strip()
+            item_text = (
+                self.ui.tableWidget_ins_apply.item(row, 8)
+                .text()
+                .replace(",", "")
+                .strip()
+            )
             apply_fee = int(item_text) if item_text.isdigit() else 0
-            data_rows.append({
-                'month': month,
-                'fee': apply_fee
-            })
+            data_rows.append({"month": month, "fee": apply_fee})
 
         # 建立 BarSet：健保申請
         fee_set = QBarSet("健保申請")
         months = []
         for row in data_rows:
-            months.append(row['month'])
-            fee_set << row['fee']
+            months.append(row["month"])
+            fee_set << row["fee"]
 
         # 建立 BarSeries 並加入資料
         series = QBarSeries()
@@ -221,7 +266,7 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
         chart.setAxisX(axis_x, series)
 
         # Y 軸：數值軸
-        max_y = max(row['fee'] for row in data_rows)
+        max_y = max(row["fee"] for row in data_rows)
         axis_y = QValueAxis()
         axis_y.setRange(0, max_y * 1.1)
         axis_y.setTitleText("健保申請金額")
@@ -236,24 +281,31 @@ class StatisticsInsApplyYear(QtWidgets.QMainWindow):
         self.ui.horizontalLayout_age_group.addWidget(chart_view)
 
     def _export_to_excel(self):
-        last_dir = system_utils.get_last_directory('健保申報分列項目表')
+        last_dir = system_utils.get_last_directory("健保申報分列項目表")
         options = QFileDialog.Options()
-        excel_filename = os.path.join(last_dir, f"{self.dialog_setting['year']}年度健保申報分項表.xlsx")
+        excel_filename = os.path.join(
+            last_dir, f"{self.dialog_setting['year']}年度健保申報分項表.xlsx"
+        )
         excel_filename, _ = QFileDialog.getSaveFileName(
             self.parent,
-            "匯出分項表",excel_filename,
-            "excel檔案 (*.xlsx);;Text Files (*.txt)", options=options
+            "匯出分項表",
+            excel_filename,
+            "excel檔案 (*.xlsx);;Text Files (*.txt)",
+            options=options,
         )
         if not excel_filename:
             return
 
         export_utils.export_table_widget_to_excel(
-            excel_filename, self.ui.tableWidget_ins_apply,
-            None, [1, 2, 3, 4, 5, 6, 7, 8], f"{self.dialog_setting['year']}健保申報年度分列項目表",
+            excel_filename,
+            self.ui.tableWidget_ins_apply,
+            None,
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            f"{self.dialog_setting['year']}健保申報年度分列項目表",
         )
         system_utils.show_message_box(
             QMessageBox.Information,
-            '資料匯出完成',
-            f'<h3>{excel_filename}匯出完成.</h3>',
-            'Microsoft Excel 格式.'
+            "資料匯出完成",
+            f"<h3>{excel_filename}匯出完成.</h3>",
+            "Microsoft Excel 格式.",
         )
