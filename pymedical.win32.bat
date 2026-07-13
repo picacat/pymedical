47e6446da115b92b1871d07b1f721f228432167f
picacat <pica.cat@gmail.com>
2026-07-14 06:14:33 +0800

    2026-07-14 v2

--- pymedical.win32.bat
+++ pymedical.win32.bat
@@ -1,10 +1,9 @@
-@echo off
 
 :: change current dir
 cd /d "%~dp0"
 
-if exist "\pymedical\venv\Scripts\activate.bat" (
-    call "\pymedical\venv\Scripts\activate.bat"
+if exist "venv\Scripts\activate.bat" (
+    call "venv\Scripts\activate.bat"
 )
 
 start /min pythonw pymedical.py
