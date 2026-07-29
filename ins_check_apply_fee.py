ff9f1554daaa049dc96518d407e807ed2fe7bff3
John Huang <pica.cat@gmail.com>
2026-07-29 12:15:51 +0800

    2026-07-29 v3

--- ins_check_apply_fee.py
+++ ins_check_apply_fee.py
@@ -26,7 +26,7 @@ from libs import (
 class InsCheckApplyFee(QtWidgets.QMainWindow):
     # 初始化
     def __init__(self, parent=None, *args):
-        super(InsCheckApplyFee, self).__init__(parent)
+        super().__init__(parent)
         self.parent = parent
         self.database = args[0]
         self.system_settings = args[1]
